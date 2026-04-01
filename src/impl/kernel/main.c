14ff039bde95ed9cabffe22697fb660da8897b0b
Ansh-droid-glitch <anshsingh131014@gmail.com>
2026-04-01 10:52:57 +0530

    printing dosent work

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,5 +1,23 @@
 #include <stdint.h>
 
+// Request start/end anchors required by Limine v5+
+__attribute__((used, section(".limine_requests_start")))
+static volatile uint64_t limine_requests_start_marker[] = {
+    0xf6b8f4b39de7d1ae, 0xfab91a6940fcb9cf
+};
+
+__attribute__((used, section(".limine_requests_end")))
+static volatile uint64_t limine_requests_end_marker[] = {
+    0x854c3a44, 0xb459a09e
+};
+
+// Base revision (tell Limine we support revision 2)
+__attribute__((used, section(".limine_requests")))
+static volatile uint64_t limine_base_revision[] = {
+    0xf9562b2d, 0x07ab71e5, 2
+};
+
+// HHDM request
 struct limine_hhdm_response {
     uint64_t revision;
     uint64_t offset;
@@ -11,7 +29,6 @@ struct limine_hhdm_request {
     struct limine_hhdm_response *response;
 };
 
-// magic nums
 __attribute__((used, section(".limine_requests")))
 static volatile struct limine_hhdm_request hhdm_request = {
     .id = {
@@ -33,9 +50,8 @@ static void print(const char *str, uint64_t hhdm_offset) {
 }
 
 void kernel_main(void) {
-    if (hhdm_request.response == 0) {
-        for (;;) __asm__("hlt"); // hang safely if limine didnt fill it
-    }
+    if (hhdm_request.response == 0)
+        for (;;) __asm__("hlt");
 
     print("Hello, World!", hhdm_request.response->offset);
 
--- src/impl/x86_64/boot/header.asm
+++ src/impl/x86_64/boot/header.asm
@@ -1,19 +1,10 @@
-section .limine_requests
-
-limine_base_revision:
-    dq 0xf9562b2d
-    dq 0x07ab71e5
-    dq 2
-
 section .text
 global _start
 extern kernel_main
 
 _start:
     cld
-    ; Point rsp to our own stack
     mov rsp, stack_top
-    ; Reset base pointer
     xor rbp, rbp
     call kernel_main
 .hang:
@@ -23,5 +14,5 @@ _start:
 section .bss
 align 16
 stack_bottom:
-    resb 16384      ; 16 KiB stack
+    resb 16384
 stack_top:
